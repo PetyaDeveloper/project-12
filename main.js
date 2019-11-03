dfb72b9facb02465d47f8a2aa5e20a8a456b42a4
PetyaDeveloper <pskpetya@gmail.com>
2019-11-03 23:11:23 +0400

    added carosel plugin for the projects and added media queries

---
+++ main.js
@@ -0,0 +1,32 @@
+ function myFunction() {
+    // Declare variables
+    var input, filter, ul, li, a, i;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('li');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+// nav links asigning class active
+const nav = document.querySelector('.nav');
+let navLink = document.querySelectorAll('.nav a');
+console.log(navLink);
+
+nav.addEventListener('click',(e)=>{
+    if(e.target.tagName === 'A'){
+      for(let i=0; i<navLink.length; i++){
+          navLink[i].removeAttribute('class');
+          e.target.className = "selected";
+      }
+    }
+});
